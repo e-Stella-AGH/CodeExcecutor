f2d1f1990c391b6816655f7ef9af22042efcd8f4
Dawid Dębowski <dawid199960@gmail.com>
2021-08-15 12:21:41 +0200

    cors

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const { CodeExecutorService } = require('./services/CodeExecutorService')
 const express = require('express')
+const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const app = express()
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 
 app.get("/", (req, res) => {
